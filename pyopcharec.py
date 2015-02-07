e9c6dc48bb61aeb2cd2a05c2f3876b75e2dc1fc6
Sergey Romanov <xxsmotur@gmail.com>
2015-02-08 01:51:20 +0500

    Some append

--- pyopcharec.py
+++ pyopcharec.py
@@ -2,9 +2,33 @@ import numpy as np
 from sklearn import metrics, svm, decomposition
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
+from sklearn.datasets import fetch_mldata
+from skimage import data, io, filter
+from skimage.transform import resize
 
 def load_digits():
 	return datasets.load_digits()
+
+def load_mnist():
+	return fetch_mldata('MNIST original', data_home='../datasets')
+
+#http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/
+def prepare_characters(path):
+	coll = io.ImageCollection('../English/Hnd/Img/Sample005/*.png')
+	print(coll[10])
+
+
+def toy_example():
+	data = load_digits()
+	train, test = split_data(data)
+	pca = decomposition.PCA(n_components=3)
+	data_pca = pca.fit_transform(data.data)
+	classifier = svm.SVC(gamma=0.001)
+	classifier.fit(data_pca, data.target)
+	cost = 0
+	for i in range(len(test)):
+		cost += classifier.predict(test[i][0])[0] == test[i][1]
+	print(cost/len(test), cost, len(test))
 
 def split_data(data, pr=.75):
 	"""
@@ -16,13 +40,3 @@ def split_data(data, pr=.75):
 	testidx = [i for i in rang if i not in trainidx.tolist()]
 	pre = [(img, target) for img, target in zip(data.data, data.target)]
 	return np.take(pre, trainidx, axis=0).tolist(), np.take(pre, testidx, axis=0).tolist()
-
-data = load_digits()
-train, test = split_data(data)
-pca = decomposition.PCA()
-classifier = svm.SVC(gamma=0.001)
-classifier.fit(list(map(lambda x: x[0], train)),  list(map(lambda x: x[1], train)))
-cost = 0
-for i in range(len(test)):
-	cost += classifier.predict(test[i][0])[0] == test[i][1]
-print(cost/len(test), cost, len(test))
